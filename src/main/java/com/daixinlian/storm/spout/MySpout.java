d73ff2025a5b772a2c6278af1d303739e3b81cde
sxikun <sxikun@daixinlian.com>
2018-02-26 17:13:40 +0800

    Integration kafka

--- src/main/java/com/daixinlian/storm/spout/MySpout.java
+++ src/main/java/com/daixinlian/storm/spout/MySpout.java
@@ -1,16 +1,14 @@
 package com.daixinlian.storm.spout;
 
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.IRichSpout;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichSpout;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Values;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
 
 import java.util.Arrays;
 import java.util.Map;
--- src/main/java/com/daixinlian/storm/topology/MyTopology.java
+++ src/main/java/com/daixinlian/storm/topology/MyTopology.java
@@ -1,28 +1,48 @@
 package com.daixinlian.storm.topology;
 
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
-import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.InvalidTopologyException;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
 import com.daixinlian.storm.bolt.MyBolt1;
 import com.daixinlian.storm.bolt.MyBolt2;
 import com.daixinlian.storm.spout.MySpout;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.kafka.*;
+import org.apache.storm.spout.SchemeAsMultiScheme;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+
+import java.util.Arrays;
 
 /**
  * Created by Duo Nuo on 2018/2/22 0022.
  */
 public class MyTopology {
 
-	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException {
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException {
 
 		Config config = new Config();
 
 
 		TopologyBuilder builder = new TopologyBuilder();
-		builder.setSpout("myspout",new MySpout(),2);
+
+		String zks = "192.168.10.22:2181,192.168.10.23:2181,192.168.10.24:2181";
+		String topic = "test";
+		String zkRoot = "/storm"; // default zookeeper root configuration for storm
+		String id = "word";
+
+		BrokerHosts brokerHosts = new ZkHosts(zks);
+		SpoutConfig spoutConf = new SpoutConfig(brokerHosts, topic, zkRoot, id);
+		spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
+//		spoutConf.forceFromStart = true;
+		spoutConf.zkServers = Arrays.asList(new String[] {"192.168.10.22","192.168.10.23","192.168.10.24"});
+		spoutConf.zkPort = 2181;
+
+		KafkaSpout kafkaSpout = new KafkaSpout(spoutConf);
+
+		builder.setSpout("myspout",kafkaSpout,3);
 		builder.setBolt("mybolt1",new MyBolt1(),2).shuffleGrouping("myspout");
 		builder.setBolt("mybolt2",new MyBolt2(),4).fieldsGrouping("mybolt1",new Fields("word"));
 
@@ -35,7 +55,5 @@ public class MyTopology {
 			cluster.submitTopology("wordcount",config,builder.createTopology());
 			System.out.println("本地调试已经启动");
 		}
-
-
 	}
 }
